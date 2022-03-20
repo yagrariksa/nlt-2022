2db669d919c3f38362c2f9f1ebd8322acad67eb0
oxysh <oxy.s.hapsari04@gmail.com>
2022-03-20 23:18:42 +0700

    mobile admin in login

--- resources/views/container/admin/masuk.blade.php
+++ resources/views/container/admin/masuk.blade.php
@@ -19,6 +19,19 @@
     </div>
 @endsection
 
+@section('admin-mobile')
+    <div class="adm-mobile__text">
+        <h1 class="adm-mobile__title">Sorry:(</h1>
+        <h2 class="adm-mobile__desc">Please use <span>laptop</span> or <span>desktop PC</span> to acces this admin page
+            for
+            ease of use. Thank You!</h2>
+    </div>
+    <div class="adm-mobile__images">
+        <img src="{{ url('assets/img/logo-amsa.png') }}" alt="" class="adm-mobile__img">
+        <img src="{{ url('assets/img/logo-nlt.png') }}" alt="" class="adm-mobile__img">
+    </div>
+@endsection
+
 @section('other')
     {{-- it can be modal, etc. --}}
 @endsection
